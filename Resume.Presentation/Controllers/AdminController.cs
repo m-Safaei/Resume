e348e0a49f8df86a8a159d534fdb0af1947d2d82
maryam safaei <maryam.safaei701@gmail.com>
2024-01-24 13:42:35 +0330

    Add static files

---
+++ Resume.Presentation/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿#region Using
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Resume.Presentation.Controllers;
+
+#endregion
+
+
+public class AdminController : Controller
+{
+    #region Admin Dashboard
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+    
+
+    #endregion
+}
+
