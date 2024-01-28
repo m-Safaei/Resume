76ad67b05200cd02a3a87f5407b681c51c9669c6
maryam safaei <maryam.safaei701@gmail.com>
2024-01-28 21:03:13 +0330

    Add Index

--- Resume.Presentation/Controllers/AdminController.cs
+++ Resume.Presentation/Controllers/AdminController.cs
@@ -15,7 +15,11 @@ public class AdminController : Controller
     {
         return View();
     }
-    
+    [HttpPost]
+    public IActionResult Index(string email,string username)
+    {
+        return View();
+    }
 
     #endregion
 }
