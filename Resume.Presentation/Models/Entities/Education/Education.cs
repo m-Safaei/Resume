3e3f61e66cb681baa017ff774883f9ad84178699
maryam safaei <maryam.safaei701@gmail.com>
2024-01-29 12:07:23 +0330

    Initial Education, Experience, MySkills Models

---
+++ Resume.Presentation/Models/Entities/Education/Education.cs
@@ -0,0 +1,10 @@
+﻿namespace Resume.Presentation.Models.Entities.Education;
+
+public class Education
+{
+    public int Id { get; set; }
+    public string EducationTitle { get; set; }
+    public string EducationDuration { get; set; }
+    public string Description { get;set; }
+}
+
