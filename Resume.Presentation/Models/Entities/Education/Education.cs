8f6355afa44041728af68ade68ea712533cddbb8
maryam safaei <maryam.safaei701@gmail.com>
2024-01-29 23:00:44 +0330

    Add Experiences, MySkills and Educations tables

--- Resume.Presentation/Models/Entities/Education/Education.cs
+++ Resume.Presentation/Models/Entities/Education/Education.cs
@@ -5,6 +5,6 @@ public class Education
     public int Id { get; set; }
     public string EducationTitle { get; set; }
     public string EducationDuration { get; set; }
-    public string Description { get;set; }
+    public string? Description { get;set; }
 }
 
---
+++ Resume.Presentation/Models/ResumeDbContext/ResumeDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Resume.Presentation.Models.Entities.Education;
+using Resume.Presentation.Models.Entities.Experience;
+using Resume.Presentation.Models.Entities.MySkills;
+
+namespace Resume.Presentation.Models.ResumeDbContext;
+
+public class ResumeDbContext:DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(
+            "Data Source=DESKTOP-M9L4136\\SQLEXPRESS;Initial Catalog=ResumeDaneshkar;Integrated Security=True;MultipleActiveResultSets=true;Trust Server Certificate=True");
+        base.OnConfiguring(optionsBuilder);
+    }
+    public DbSet<Experience> Experiences { get; set; }
+    public DbSet<MySkills> MySkills { get; set; }
+    public DbSet<Education> Educations { get; set; }
+}
+
