e698c31921d4b3066f9b895dff62d7c7186fceea
maryam safaei <maryam.safaei701@gmail.com>
2024-01-31 23:03:29 +0330

    End of Education Class

--- Resume.Presentation/Controllers/EducationController.cs
+++ Resume.Presentation/Controllers/EducationController.cs
@@ -1,12 +1,59 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿#region Usings
 
-namespace Resume.Presentation.Controllers
+using Microsoft.AspNetCore.Mvc;
+using Resume.Presentation.Models.Entities.Education;
+using Resume.Presentation.Models.ResumeDbContext;
+
+namespace Resume.Presentation.Controllers;
+
+
+#endregion
+public class EducationController : Controller
 {
-    public class EducationController : Controller
+    private readonly ResumeDbContext _context;
+
+    #region Ctor
+
+    public EducationController(ResumeDbContext context)
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        _context = context;
     }
+
+    #endregion
+
+    #region List of Educations
+    [HttpGet]
+    public IActionResult ListOfEducations()
+    {
+        List<Education> educations = _context.Educations.ToList();
+        var education = _context.Educations.OrderBy(e=>e.Id).Last();
+        return View();
+    }
+
+    #endregion
+
+    #region Create an Education
+
+    public IActionResult CreateEducation()
+    {
+        #region Create Record
+
+        Education education = new()
+        {
+            EducationTitle = "Master",
+            EducationDuration = "2012",
+            Description = "Degree"
+        };
+
+        _context.Educations.Add(education);
+        _context.SaveChanges();
+
+
+        #endregion
+        return View();
+    }
+    
+
+    #endregion
 }
+
